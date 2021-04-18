a28b16c87ae1404a1f523f67af7d8820d7b534e5
ssz1997 <ss1060@duke.edu>
2021-04-18 16:16:29 -0400

    Create new contact

--- NunnLabContacts/NunnLabContacts/createContactViewController.swift
+++ NunnLabContacts/NunnLabContacts/createContactViewController.swift
@@ -11,16 +11,69 @@
 
 import UIKit
 
-class createContactViewController: UIViewController {
-
+class createContactViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    
+    @IBOutlet weak var name: UITextField!
+    @IBOutlet weak var sex: UITextField!
+    @IBOutlet weak var location: UITextField!
+    @IBOutlet weak var alias: UITextField!
+    @IBOutlet weak var birthday: UITextField!
+    @IBOutlet weak var submit: UIButton!
+    var aliasArray = [String]()
+    var nameStr: String = ""
+    var sexStr: String?
+    var locationStr: String?
+    var aliasStr: String?
+    var birthdayStr: String?
+    var imageData: Data?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+    }
+    @IBAction func uploadPhoto(_ sender: Any) {
+        let imagePickerControlller = UIImagePickerController()
+        imagePickerControlller.delegate = self
+        imagePickerControlller.sourceType = .photoLibrary
+        self.present(imagePickerControlller, animated: true, completion: nil)
 
-        // Do any additional setup after loading the view.
+    }
+    @IBAction func takePhoto(_ sender: Any) {
+        let imagePickerControlller = UIImagePickerController()
+        imagePickerControlller.delegate = self
+        if UIImagePickerController.isSourceTypeAvailable(.camera) {
+            imagePickerControlller.sourceType = .camera;
+            self.present(imagePickerControlller, animated: true, completion: nil)
+        } else {
+            print("Camera not available")
+        }
     }
     
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        let image = info[.originalImage] as! UIImage
+        picker.dismiss(animated: true, completion: nil)
+        imageData = image.pngData()
+    }
 
-    /*
+    @IBAction func createContact(_ sender: Any) {
+        nameStr = name.text!.trimmingCharacters(in: .whitespaces)
+        sexStr = sex.text
+        locationStr = location.text
+        aliasStr = alias.text
+        birthdayStr = birthday.text
+        if aliasStr != nil && aliasStr!.count != 0 {
+            aliasArray = aliasStr!.components(separatedBy: ", ")
+        }
+        if nameStr.count != 0 {
+            PersistenceManager.shared.insertContact(name: nameStr, aliases: aliasArray, birthdate: birthdayStr, sex: sexStr, location: locationStr, picture: imageData)
+            submit.setTitle("Success!", for: .normal)
+            name.layer.borderWidth = 0
+        } else {
+            name.layer.borderWidth = 1
+            name.layer.borderColor = UIColor.red.cgColor
+        }
+    }
+    /*s
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
@@ -29,5 +82,6 @@ class createContactViewController: UIViewController {
         // Pass the selected object to the new view controller.
     }
     */
+    
 
 }
