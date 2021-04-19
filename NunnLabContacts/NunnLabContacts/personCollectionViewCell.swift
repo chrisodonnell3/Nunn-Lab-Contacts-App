9cfb927e547f52d3a52c9760a10ba69ac9b4343b
ayw9 <ayw9@duke.edu>
2021-04-19 13:13:03 -0400

    added launch screen + design changes

--- NunnLabContacts/NunnLabContacts/personCollectionViewCell.swift
+++ NunnLabContacts/NunnLabContacts/personCollectionViewCell.swift
@@ -20,16 +20,16 @@ class personCollectionViewCell: UICollectionViewCell {
         contactPhoto.layer.borderWidth = 1
         contactPhoto.layer.masksToBounds = false
         contactPhoto.layer.borderColor = UIColor.clear.cgColor
-        contactPhoto.layer.cornerRadius = contactPhoto.frame.height/2
+        //contactPhoto.layer.cornerRadius = contactPhoto.frame.height/2
         contactPhoto.clipsToBounds = true
         
         // Modify the layout of the cell
-        self.layer.cornerRadius = 16.0
+        //self.layer.cornerRadius = 16.0
         self.layer.borderWidth = 5.0
         self.layer.borderColor = UIColor.clear.cgColor
         self.layer.masksToBounds = true
     
-        self.contentView.layer.cornerRadius = 15.0
+        //self.contentView.layer.cornerRadius = 15.0
         self.contentView.layer.borderWidth = 5.0
         self.contentView.layer.borderColor = UIColor.clear.cgColor
         self.contentView.layer.masksToBounds = true
@@ -37,7 +37,7 @@ class personCollectionViewCell: UICollectionViewCell {
         self.layer.shadowOffset = CGSize(width: 0, height: 0.0)
         self.layer.shadowRadius = 3.5
         self.layer.shadowOpacity = 0.6
-        self.layer.cornerRadius = 15.0
+        //self.layer.cornerRadius = 15.0
         self.layer.masksToBounds = false
         self.layer.shadowPath = UIBezierPath(roundedRect: self.bounds, cornerRadius: self.contentView.layer.cornerRadius).cgPath
     
